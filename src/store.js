3fdd1ddaf73b218e9f1eec9e2c13dbb09ddce3b1
ngerek <ngere.k@outlook.com>
2024-08-02 23:02:59 +0100

    Included newMoviesReducer in store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import newMoviesReducer from "./features/movies/newMoviesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    newMovies: newMoviesReducer,
+  },
 });
