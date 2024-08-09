5a4447437095c962b3d3c42996c19b62f5668e2d
ngerek <ngere.k@outlook.com>
2024-08-09 23:49:03 +0100

    added userReducer & modalReducer in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import newMoviesReducer from "./features/movies/newMoviesSlice";
+import userReducer from "./features/user/userSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     newMovies: newMoviesReducer,
+    user: userReducer,
+    modal: modalReducer,
   },
 });
