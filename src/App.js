1dd2d4bc7075d310793614fb5faf9bde0adf9554
ngerek <ngere.k@outlook.com>
2024-08-11 23:38:06 +0100

    more App.js setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { userStateChanged } from "./features/user/userSlice";
+import { auth } from "./firebase/config";
+import { useEffect } from "react";
 
 // components
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Login from "./components/login/Login";
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 
 // pages
 import Home from "./pages/home/Home";
@@ -16,23 +24,42 @@ import SeriesDetail from "./pages/seriesDetail/SeriesDetail";
 
 // styles
 import "./App.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const { isLoginOpen } = useSelector((store) => store.modal);
+  const { isAuthReady } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      dispatch(userStateChanged(authUser));
+      unsubscribe();
+    });
+  }, [dispatch]);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="series" element={<Series />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="search" element={<Search />} />
-        <Route path="/movie-detail/:id" element={<MovieDetail />} />
-        <Route path="series-detail/:id" element={<SeriesDetail />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div>
+      {isAuthReady && (
+        <BrowserRouter>
+          <Navbar />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="series" element={<Series />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="search" element={<Search />} />
+            <Route path="movie-detail/:id" element={<MovieDetail />} />
+            <Route path="series-detail/:id" element={<SeriesDetail />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+          {isLoginOpen && <Login />}
+          <ToastContainer position="top-center" className="toastStyle" />
+        </BrowserRouter>
+      )}
+    </div>
   );
 }
 
