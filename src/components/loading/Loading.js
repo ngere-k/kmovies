6b0188e8776f5a0f597fa0844296f2cc4ae2d2f5
ngerek <ngere.k@outlook.com>
2024-08-15 23:46:02 +0100

    built loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,12 @@
+// styles
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="section section-center">
+      <div className="loading" />
+    </div>
+  );
+};
+
+export default Loading;
