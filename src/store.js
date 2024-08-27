764e6a8ae2ad7d21348e81d54c2b40127e47b253
ngerek <ngere.k@outlook.com>
2024-08-27 23:06:29 +0100

    added searchReducer in store.js

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import newMoviesReducer from "./features/movies/newMoviesSlice";
 import userReducer from "./features/user/userSlice";
 import modalReducer from "./features/modal/modalSlice";
+import searchReducer from "./features/search/searchSlice";
 
 export const store = configureStore({
   reducer: {
     newMovies: newMoviesReducer,
     user: userReducer,
     modal: modalReducer,
+    search: searchReducer,
   },
 });
