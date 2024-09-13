6b100f9ea716d3fb2ea579b027af1910c0fcd6f6
ngerek <ngere.k@outlook.com>
2024-09-13 23:55:55 +0100

    created detailsSlice.js

---
+++ src/features/details/detailsSlice.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import customAxios from "../../utils/axios";
+
+const initialState = {
+  credits: [],
+  keywords: [],
+};
+
+export const fetchCredits = createAsyncThunk(
+  "details/fetchCredits",
+  async ({ type, id }, { rejectWithValue }) => {
+    try {
+      const res = await customAxios.get(`/${type}/${id}/credits`);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      //   return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchKeywords = createAsyncThunk(
+  "details/fetchKeywords",
+  async ({ type, id }, { rejectWithValue }) => {
+    try {
+      const res = await customAxios.get(`/${type}/${id}/keywords`);
+      return res.data.keywords;
+    } catch (error) {
+      console.log(error);
+      //   return rejectWithValue(error.message);
+    }
+  }
+);
+
+const detailsSlice = createSlice({
+  name: "details",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCredits.fulfilled, (state, { payload }) => {
+        state.credits = payload;
+      })
+      //   .addCase(fetchCredits.rejected, (state, { payload }) => {
+      //     state.error = payload;
+      //   })
+
+      .addCase(fetchKeywords.fulfilled, (state, { payload }) => {
+        state.keywords = payload;
+      });
+    //   .addCase(fetchKeywords.rejected, (state, { payload }) => {
+    //     state.error = payload;
+    //   });
+  },
+});
+
+const detailsReducer = detailsSlice.reducer;
+export default detailsReducer;
