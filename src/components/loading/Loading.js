c4706d97a702567b3ea9b7423574740293bbeb0f
ngerek <ngere.k@outlook.com>
2024-09-17 19:02:11 +0100

    fullPageLoading prop & condition

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,7 +1,15 @@
 // styles
 import "./Loading.scss";
 
-const Loading = () => {
+const Loading = ({ fullPageLoading }) => {
+  if (fullPageLoading) {
+    return (
+      <div className="full-page-loading">
+        <div className="loading" />
+      </div>
+    );
+  }
+
   return (
     <div className="section section-center">
       <div className="loading" />
