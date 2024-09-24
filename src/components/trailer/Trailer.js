a752b12e1f6070ddd417e82c4060e0f64e7b8ce8
ngerek <ngere.k@outlook.com>
2024-09-24 23:25:24 +0100

    youtube logic

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -1,10 +1,28 @@
+import YouTube from "react-youtube";
+
 // styles
-import './Trailer.scss'
+import "./Trailer.scss";
 
-const Trailer = () => {
+const Trailer = ({ trailerKey }) => {
+  const opts = {
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const onReady = (event) => {
+    event.target.pauseVideo();
+  };
+
   return (
-    <div>Trailer</div>
-  )
-}
+    <YouTube
+      videoId={trailerKey}
+      opts={opts}
+      onReady={onReady}
+      className="youtube-container"
+      iframeClassName="youtube-iframe"
+    />
+  );
+};
 
-export default Trailer
+export default Trailer;
