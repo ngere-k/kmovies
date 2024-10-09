0b48c0f11aeb59e6d604a1327fa2c760fd537b4e
ngerek <ngere.k@outlook.com>
2024-10-09 21:23:04 +0100

    built Recommendations.js

--- src/components/recommendations/Recommendations.js
+++ src/components/recommendations/Recommendations.js
@@ -1,8 +1,29 @@
+import Card from "../card/Card";
+
 // styles
 import "./Recommendations.scss";
 
-const Recommendations = () => {
-  return <div>Recommendations</div>;
+const Recommendations = ({ recommendations }) => {
+  return (
+    <section className="section-recommendations">
+      <div className="container">
+        {recommendations.length > 0 && (
+          <h2 className="heading-secondary">you may also like...</h2>
+        )}
+        <div className="grid-col-4">
+          {recommendations.slice(0, 12).map((movie) => {
+            return (
+              <Card
+                key={movie.id}
+                {...movie}
+                path={movie.media_type === "movie" ? "/movie" : "/series-info"}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Recommendations;
