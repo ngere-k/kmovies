23090505d3a532ef7909af78c5c2650f45d5dc44
ngerek <ngere.k@outlook.com>
2024-10-12 21:17:28 +0100

    setup link and routes

--- src/components/discover/Discover.js
+++ src/components/discover/Discover.js
@@ -1,8 +1,40 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+// components
+import Casts from "../casts/Casts";
+import Reviews from "../reviews/Reviews";
+import Photos from "../photos/Photos";
+
 // styles
 import "./Discover.scss";
 
-const Discover = () => {
-  return <div>Discover</div>;
+const Discover = ({ credits, reviews, images }) => {
+  return (
+    <section className="section-discover">
+      <div className="container">
+        <nav className="discover">
+          <ul>
+            <li className="discover__link">
+              <Link to="reviews">Reviews</Link>
+            </li>
+            <li className="discover__link">
+              <Link to="casts">Casts</Link>
+            </li>
+            <li className="discover__link">
+              <Link to="photos">Photos</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index path="/" element={<Reviews reviews={reviews} />} />
+          <Route path="reviews" element={<Reviews reviews={reviews} />} />
+          <Route path="casts" element={<Casts credits={credits} />} />
+          <Route path="photos" element={<Photos images={images} />} />
+        </Routes>
+      </div>
+    </section>
+  );
 };
 
 export default Discover;
