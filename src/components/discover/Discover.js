11aae0124a747aed5b9d2b0538d53b35c407128a
ngerek <ngere.k@outlook.com>
2024-10-18 23:14:39 +0100

    links and routes

--- src/components/discover/Discover.js
+++ src/components/discover/Discover.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 // components
 import Casts from "../casts/Casts";
@@ -11,27 +11,51 @@ import "./Discover.scss";
 const Discover = ({ credits, reviews, images }) => {
   return (
     <section className="section-discover">
-      <div className="container">
-        <nav className="discover">
-          <ul>
-            <li className="discover__link">
-              <Link to="reviews">Reviews</Link>
+      <div className="discover container">
+        <h2 className="heading-secondary">Discover</h2>
+        <nav className="discover__nav">
+          <ul className="discover__lists">
+            <li className="discover__list">
+              <NavLink
+                to="reviews"
+                className={({ isActive }) =>
+                  isActive ? "discover__link active" : "discover__link"
+                }
+              >
+                Reviews
+              </NavLink>
             </li>
-            <li className="discover__link">
-              <Link to="casts">Casts</Link>
+            <li className="discover__list">
+              <NavLink
+                to="casts"
+                className={({ isActive }) =>
+                  isActive ? "discover__link active" : "discover__link"
+                }
+              >
+                Casts
+              </NavLink>
             </li>
-            <li className="discover__link">
-              <Link to="photos">Photos</Link>
+            <li className="discover__list">
+              <NavLink
+                to="photos"
+                className={({ isActive }) =>
+                  isActive ? "discover__link active" : "discover__link"
+                }
+              >
+                Photos
+              </NavLink>
             </li>
           </ul>
         </nav>
 
-        <Routes>
-          <Route index path="/" element={<Reviews reviews={reviews} />} />
-          <Route path="reviews" element={<Reviews reviews={reviews} />} />
-          <Route path="casts" element={<Casts credits={credits} />} />
-          <Route path="photos" element={<Photos images={images} />} />
-        </Routes>
+        <div className="discover__content">
+          <Routes>
+            <Route index path="/" element={<Reviews reviews={reviews} />} />
+            <Route path="reviews" element={<Reviews reviews={reviews} />} />
+            <Route path="casts" element={<Casts credits={credits} />} />
+            <Route path="photos" element={<Photos images={images} />} />
+          </Routes>
+        </div>
       </div>
     </section>
   );
