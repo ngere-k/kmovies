76248fefa7f11780e8f48c11b530f01136c84fc2
ngerek <ngere.k@outlook.com>
2024-10-21 23:45:07 +0100

    built Casts.js

--- src/components/casts/Casts.js
+++ src/components/casts/Casts.js
@@ -1,9 +1,30 @@
+import { imageUrl } from "../../utils/imageUrl";
+import Avatar from "../../assets/avatar.jpg";
+
 // styles
 import "./Casts.scss";
 
-const Casts = ({ credits }) => {
-  console.log(credits);
-  return <div>Casts</div>;
+const Casts = ({ credits: { cast } }) => {
+  return (
+    <article className="cast-article">
+      {cast?.slice(0, 20).map((item) => {
+        const { id, profile_path, name, character } = item;
+        return (
+          <div key={id} className="cast">
+            <figure className="cast__figure">
+              <img
+                src={profile_path ? `${imageUrl}/${profile_path}` : Avatar}
+                alt={name}
+                className="cast__avatar"
+              />
+            </figure>
+            <h4 className="heading-quaternary cast__name">{name}</h4>
+            <p className="cast__character">{character}</p>
+          </div>
+        );
+      })}
+    </article>
+  );
 };
 
 export default Casts;
