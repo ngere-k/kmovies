b7b50a2eba2db6fdbbc050231e4f7c139c17a08b
ngerek <ngere.k@outlook.com>
2024-10-21 23:48:58 +0100

    built Photos.js

--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -1,9 +1,24 @@
+import { imageUrl } from "../../utils/imageUrl";
+
 // styles
 import "./Photos.scss";
 
-const Photos = ({ images }) => {
-  console.log(images);
-  return <div>Photos</div>;
+const Photos = ({ images: { backdrops } }) => {
+  return (
+    <article className="image-article">
+      {backdrops?.slice(0, 12).map((backdrop) => {
+        return (
+          <figure key={backdrop.file_path} className="backdrop__figure">
+            <img
+              src={`${imageUrl}/${backdrop.file_path}`}
+              alt="backdrop image"
+              className="backdrop__image"
+            />
+          </figure>
+        );
+      })}
+    </article>
+  );
 };
 
 export default Photos;
