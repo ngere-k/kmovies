109d4e1d846ca73b919eafe6abcf7784b22f8503
ngerek <ngere.k@outlook.com>
2024-11-02 22:58:27 +0100

    built Season.js

--- src/components/seasons/Seasons.js
+++ src/components/seasons/Seasons.js
@@ -1,8 +1,23 @@
+import Card from "../card/Card";
+
 // styles
 import "./Seasons.scss";
 
 const Seasons = ({ seasons }) => {
-  return <h2>Seasons</h2>;
+  console.log(seasons);
+  return (
+    <section className="container">
+      <h2 className="heading-secondary">seasons</h2>
+
+      <div className="season grid-col-4">
+        {seasons
+          ?.filter((season) => season.name !== "Specials")
+          .map((season) => {
+            return <Card key={season.id} {...season} />;
+          })}
+      </div>
+    </section>
+  );
 };
 
 export default Seasons;
