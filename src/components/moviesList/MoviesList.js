f64aeb36638763a34a4d9383eb1c08d7e234a764
ngerek <ngere.k@outlook.com>
2024-11-22 20:16:54 +0100

    built MoviesList.js

--- src/components/moviesList/MoviesList.js
+++ src/components/moviesList/MoviesList.js
@@ -1,8 +1,28 @@
+import { useSelector } from "react-redux";
+
+// components
+import Card from "../card/Card";
+import ListView from "../listView/ListView";
+
 // styles
 import "./MoviesList.scss";
 
-const MoviesList = () => {
-  return <div>MoviesList</div>;
+const MoviesList = ({ movies, path }) => {
+  const { grid_view } = useSelector((store) => store.discover);
+
+  if (!grid_view) {
+    return <ListView movies={movies} path={path} />;
+  }
+
+  return (
+    <section className="section-grid-view">
+      <div className="grid-col-3">
+        {movies.results?.map((movie) => {
+          return <Card {...movie} key={movie.id} path={path} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default MoviesList;
