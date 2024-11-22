530ab9c5cb348ce12c7e91b59930e7d441c31232
ngerek <ngere.k@outlook.com>
2024-11-22 20:14:42 +0100

    built Movies.js logic

--- src/pages/movies/Movies.js
+++ src/pages/movies/Movies.js
@@ -1,8 +1,49 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  changePage,
+  fetchDiscoverMovies,
+} from "../../features/discover/discoverSlice";
+
+// components
+import MoviesDiscover from "../../components/moviesDiscover/MoviesDiscover";
+import Loading from "../../components/loading/Loading";
+
 // styles
 import "./Movies.scss";
 
 const Movies = () => {
-  return <h2>Movies</h2>;
+  const {
+    all_movies: movies,
+    page,
+    pageCount,
+    isLoading,
+  } = useSelector((store) => store.discover);
+  const dispatch = useDispatch();
+
+  const handlePageClick = (event) => {
+    dispatch(changePage(event.selected));
+  };
+
+  useEffect(() => {
+    dispatch(fetchDiscoverMovies({ page }));
+  }, [page]);
+
+  if (isLoading) {
+    return <Loading fullPageLoading />;
+  }
+
+  return (
+    <article className="article-movies mgtp-md">
+      <MoviesDiscover
+        movies={movies}
+        path="/movie"
+        page={page}
+        pageCount={pageCount}
+        handlePageClick={handlePageClick}
+      />
+    </article>
+  );
 };
 
 export default Movies;
